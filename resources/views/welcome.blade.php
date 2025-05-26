1df8b4d4b28ba99069e6a912ef7a917f848be6aa
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-05-26 22:38:22 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    
+    <!-- Link CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="container">
+        <h1>Green Basket🌾</h1>
+    </div>
+
+    <!-- Link JS -->
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
