e6ce56a4a33b2a4b9a3686399275c021d842faa7
DIP-SARKER <sarker15-5274@diu.edu.bd>
2025-06-16 22:59:17 +0600

    Your commit message

---
+++ resources/views/shop/contact.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Contact Us</title>
+</head>
+<body>
+    <h1>Contact Page</h1>
+    <p>You can reach us at contact@example.com</p>
+
+    <a href="{{ route('home') }}">Go to Home</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('shop.welcome');
+})->name('home');
+
+
+Route::get('/contact', function () {
+    return view('shop.contact');
+})->name('contact');
+
