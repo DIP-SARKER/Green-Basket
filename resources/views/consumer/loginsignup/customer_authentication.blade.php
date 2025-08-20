3832627fd26979e3322741fae1eea3dfb4e918e4
sakib5728 <khan15-5728@diu.edu.bd>
2025-08-20 20:00:15 +0600

    update

--- resources/views/consumer/loginsignup/customer_authentication.blade.php
+++ resources/views/consumer/loginsignup/customer_authentication.blade.php
@@ -1,7 +1,7 @@
 @extends('index')
 
 @push('style')
-<title>Customer Authentication | Green Basket | Farm to Table</title>
+<title>গ্রাহক প্রমাণীকরণ | গ্রীন বাস্কেট | ফার্ম টু টেবিল</title>
 <link rel="stylesheet" href="{{ asset('css/hridoy/customer_auth.css') }}">
 @endpush
 
@@ -9,35 +9,33 @@
 
 <div class="auth-container">
     <div id="register-form" class="auth-form ">
-        <h2>Register</h2>
+        <h2>রেজিস্টার</h2>
         <form method="POST" action="{{ url('/register') }}">
             @csrf
-            <input type="text" name="name" placeholder="Name" required> <br>
-            <input type="tel" name="phone" placeholder="Phone" required> <br>
-            <input type="email" name="email" placeholder="Email" required> <br>
-            <input type="text" name="address" placeholder="Address" required> <br>
-            <input type="password" name="password" placeholder="Password" required> <br>
-            <input type="password" name="password_confirmation" placeholder="Confirm Password" required> <br>
-            <button type="submit">Register</button>
+            <input type="text" name="name" placeholder="নাম" required> <br>
+            <input type="tel" name="phone" placeholder="ফোন" required> <br>
+            <input type="email" name="email" placeholder="ইমেইল" required> <br>
+            <input type="text" name="address" placeholder="ঠিকানা" required> <br>
+            <input type="password" name="password" placeholder="পাসওয়ার্ড" required> <br>
+            <input type="password" name="password_confirmation" placeholder="পাসওয়ার্ড নিশ্চিত করুন" required> <br>
+            <button type="submit">রেজিস্টার</button>
         </form>
-        <span class="toggle-link" id="show-login">Already have an account? Login</span>
+        <span class="toggle-link" id="show-login">আগেই অ্যাকাউন্ট আছে? লগইন করুন</span>
     </div>
 
     <div id="login-form" class="auth-form active">
-        <h2>Login</h2>
+        <h2>লগইন</h2>
         <form method="POST" action="{{ url('/customer/login') }}">
             @csrf
-            <input type="tel" name="phone" placeholder="Phone" required><br>
-            <input type="password" name="password" placeholder="Password" required> <br>
-            <button type="submit">Login</button>
+            <input type="tel" name="phone" placeholder="ফোন" required><br>
+            <input type="password" name="password" placeholder="পাসওয়ার্ড" required> <br>
+            <button type="submit">লগইন</button>
         </form>
-        <span class="toggle-link" id="show-register">Don't have an account? Register</span>
+        <span class="toggle-link" id="show-register">অ্যাকাউন্ট নেই? রেজিস্টার করুন</span>
     </div>
 </div>
 
-<script src="{{ asset('js/hridoy/customer_auth.js') }}">
-
-</script>
+<script src="{{ asset('js/hridoy/customer_auth.js') }}"></script>
 
 @push('script')
 <script>
